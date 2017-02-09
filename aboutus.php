964f3c959d162c8804b7a97c4b186eeb499abbd6
DASPC\Trin <trintin7@gmail.com>
2017-02-09 23:16:51 +0700

    Updated details in AboutUs;

--- aboutus.php
+++ aboutus.php
@@ -20,38 +20,37 @@
       <h1>About Massivesoft</h1><br>
     </div>
     <div class="row">
-      <div class="col-sm-9">
+      <div class="col">
         <h1 style ="color: lightgray ; position: absolute; top: 50px; left: 30; width: 100%" >We believe in what<br>people make Possible!</h1>
         <h3 style =" color: lightgray ; position: absolute; top: 150px; left: 30; width: 100%" >Our mission is to empower every person<br>and every organization on the planet to<br>achieve more.</h3>
         <img src="/Massivesoft/content/img/camp.jpg" width="1100" height="500" class="img-fluid rounded" alt="TextBackground">
       </div>
     </div><br>
     <div class="row jumbotron">
-      <div class="col-md-6">
+      <div class="col">
         <h2 style=" font-size: 50px;">Our Company</h2><br>
-          <ul>
-            <li> 
-              <p class="lead">Massivesoft is a multinational computer technology company. The history of Massivesoft began on April 1, 1984, when it was founded by Mill Bates and Gaul Pallen in New York. Its current best-selling products are the Massivesoft Doors operating system, Massivesoft Office suite of productivity software, Pbox, a line of entertainment of games, music and video and Bing-go, a line of search engines.</p>
-            </li>
-          </ul>
+        <p><span class="lead">Massivesoft</span> is a multinational computer technology company. The history of Massivesoft began on April 1, 1984, when it was founded by Mill Bates and Gaul Pallen in New York. Its current best-selling products are the Massivesoft Doors operating system, Massivesoft Office suite of productivity software, Pbox, a line of entertainment of games, music and video and Bing-go, a line of search engines.</p>
       </div>
-      <div class="col-md-5">
-        <img src="/Massivesoft/content/img/MSbuilding.jpg" width="550" class="img-responsive"/>
+      <div class="col">
+        <img src="/Massivesoft/content/img/MSbuilding.jpg" width="550" class="img-responsive rounded"/>
       </div>
     </div>
     <div class="row jumbotron">
       <div class="row">
         <div class="col-md-4">
           <img src="/Massivesoft/content/img/ross.jpg" width="200" class="img-responsive center-block rounded-circle" alt="Massivesoft Partner" style="margin: 0 auto;"><hr>
-          <p class="lead">Originally from Hyderabad, Washington. He earned a bachelor’s degree in electrical engineering from Mangalore University, a master’s degree in computer science from the University of Wisconsin – Milwaukee and a master’s degree in business administration from the University of Chicago. Nadella serves on the board of trustees to Fred Hutchinson Cancer Research Center.  </p>
+          <h3>Satya Nadella</h3>
+          <p>Originally from Hyderabad, India, Nadella lives in Bellevue, Washington. He earned a bachelor’s degree in electrical engineering from Mangalore University, a master’s degree in computer science from the University of Wisconsin – Milwaukee and a master’s degree in business administration from the University of Chicago. Nadella serves on the board of trustees to Fred Hutchinson Cancer Research Center.</p>
         </div>
         <div class="col-md-4">
-          <img src="/Massivesoft/content/img/trin.jpg" width="200" class="img-responsive center-block rounded-circle" alt="Massivesoft Partner" style="margin: 0 auto;"><hr>
-          <p class="lead">As executive vice president of the Worldwide Commercial Business organization, Judson Althoff leads Microsoft’s commercial business strategy, including enterprise, public sector, small and mid-market, services, developer and partner. His organization is responsible for leading the digital transformation for commercial customers and partners to realize greater business value through the use of technology.</p>
+          <img src="/Massivesoft/content/img/trin.jpg" width="200" class="img-responsive center-block rounded-circle" alt="Massivesoft Partner" style="margin: 0 auto;"><hr>          
+          <h3>Judson Althoff</h3>
+          <p>As executive vice president of the Worldwide Commercial Business organization, Judson Althoff leads Microsoft’s commercial business strategy, including enterprise, public sector, small and mid-market, services, developer and partner. His organization is responsible for leading the digital transformation for commercial customers and partners to realize greater business value through the use of technology.</p>
         </div>
         <div class="col-md-4">
           <img src="/Massivesoft/content/img/joseph.jpg" width="200" class="img-responsive center-block rounded-circle" alt="Massivesoft Partner" style="margin: 0 auto;"><hr>
-          <p class="lead">As chief marketing officer, Chris Capossela leads Microsoft’s worldwide marketing and consumer business. This includes advertising, brand, communications, research as well as Microsoft’s retail stores.His passion for technology and computing began when, as a boy, he wrote a reservation system for his family’s small Italian restaurant in Boston using dBASE for DOS on an early IBM PC.</p>
+          <h3>Chris Capossela</h3>
+          <p>As chief marketing officer, Chris Capossela leads Microsoft’s worldwide marketing and consumer business. This includes advertising, brand, communications, research as well as Microsoft’s retail stores.His passion for technology and computing began when, as a boy, he wrote a reservation system for his family’s small Italian restaurant in Boston using dBASE for DOS on an early IBM PC.</p>
         </div>
       </div>
     </div><hr>
